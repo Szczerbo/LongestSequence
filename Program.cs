0b05fcd50a3493d62b8c1b89025c35be8f593936
Szczerbo <30406283+Szczerbo@users.noreply.github.com>
2022-10-25 09:38:59 +0200

    for loop overflow problem fixed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 
-List<int> inputList = new List<int> { 100, 4, 200, 1, 3, 201, 202, 203, 204, 2, 5 };
+List<int> inputList = new List<int> { 100, 4, 200, 1, 3, 201, 202, 203, 204, 2, 5, 2147483647, 2147483642, 2147483643, 2147483644, 2147483645, 2147483646, 2147483641 };
 var finalResult = LongestSequence(inputList);
 StringBuilder finalResultString = new StringBuilder();
 
@@ -15,13 +15,13 @@ static List<int> LongestSequence(List<int> input)
     List<int> basicList = new List<int>();
     List<int> pretenderList = new List<int>();
 
-    for (int i = 0; i < Int32.MaxValue; i++)
+    for (uint i = 0; i <= Int32.MaxValue; i++)
     {
-        if(input.IndexOf(i) != -1)
+        if (input.IndexOf((int)i) != -1)
         {
-            pretenderList.Add(i);
+            pretenderList.Add((int)i);
 
-            if(pretenderList.Count > basicList.Count)
+            if (pretenderList.Count > basicList.Count)
             {
                 basicList = new List<int>(pretenderList);
             }
